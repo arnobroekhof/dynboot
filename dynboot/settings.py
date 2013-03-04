64a6e83b01d27b94827349cf23aad8185177e7ff
Arno Broekhof <arno.broekhof@techop.nl>
2013-03-04 14:34:28 +0100

    Added new setting and repo

--- dynboot/settings.py
+++ dynboot/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/opt/dynboot/db/db.sqlite',                      # Or path to database file if using sqlite3.
+        'NAME': '/home/arno/Documents/dynboot/db/db.sqlite',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
         'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
@@ -45,7 +45,7 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = '/opt/dynboot/media'
+MEDIA_ROOT = '/home/arno/Documents/dynboot/media'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
@@ -56,7 +56,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/opt/dynboot/static'
+STATIC_ROOT = '/home/arno/Documents/dynboot/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -121,6 +121,7 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'pxe',
+    'repo',
 )
 
 # A sample logging configuration. The only tangible logging
